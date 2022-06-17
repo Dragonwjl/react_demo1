4ee3b5ffa5ff1e1f826baa113aaa12d57b93a047
Dragonwjl <1591531775@qq.com>
2022-06-17 18:47:48 +0800

    init

--- src/components/hello.js
+++ src/components/hello.js
@@ -3,6 +3,7 @@ import React from "react";
 
 const Hello = () => {
     return <h1> hello world</h1>
+    
 }
 
 export default Hello
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css';
 import Hello from './components/hello'
+import Body from './components/body';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-root.render(<Hello/>);
+root.render(<Body/>);
 
